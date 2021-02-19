58b5a1477cc560d728f67f303c52a75a11cc297d
Juan Sebastián Frásica G <juansebastianfrasica@hotmail.com>
2021-02-19 13:27:26 -0500

    Capa de persistencia

---
+++ src/main/java/edu/eci/ieti/rentopolis/persistence/impl/RentopolisPersistenceImpl.java
@@ -0,0 +1,25 @@
+package edu.eci.ieti.rentopolis.persistence.impl;
+
+import edu.eci.ieti.rentopolis.entities.User;
+import edu.eci.ieti.rentopolis.persistence.RentopolisPersistence;
+import edu.eci.ieti.rentopolis.persistence.RentopolisPersistenceException;
+
+import java.util.List;
+
+public class RentopolisPersistenceImpl implements RentopolisPersistence {
+
+    @Override
+    public void addUser(User user) throws RentopolisPersistenceException {
+
+    }
+
+    @Override
+    public List<User> getAllUsers() throws RentopolisPersistenceException {
+        return null;
+    }
+
+    @Override
+    public User getUserById(int id) throws RentopolisPersistenceException {
+        return null;
+    }
+}

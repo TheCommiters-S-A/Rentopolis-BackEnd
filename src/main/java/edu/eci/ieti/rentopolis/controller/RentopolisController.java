ebe4a3f63b309830a486b0a91e06aa75336ddfcf
Juan Sebastián Frásica G <juansebastianfrasica@hotmail.com>
2021-02-19 13:28:52 -0500

    Controlador

---
+++ src/main/java/edu/eci/ieti/rentopolis/controller/RentopolisController.java
@@ -0,0 +1,58 @@
+package edu.eci.ieti.rentopolis.controller;
+
+
+import edu.eci.ieti.rentopolis.entities.User;
+import edu.eci.ieti.rentopolis.services.RentopolisServicesException;
+import edu.eci.ieti.rentopolis.services.RentopolisServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/home")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class RentopolisController {
+
+
+    @Autowired
+    private RentopolisServices rentopolisServices;
+
+
+    @PostMapping("/users")
+    public ResponseEntity<?> addUser(@RequestBody User user) {
+        try {
+            rentopolisServices.addUser(user);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (RentopolisServicesException e) {
+            Logger.getLogger(RentopolisController.class.getName()).log(Level.SEVERE, e.getMessage(), e);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<?> getUsers() {
+        try {
+            return new ResponseEntity<>(rentopolisServices.getAllUsers(), HttpStatus.ACCEPTED);
+        } catch (RentopolisServicesException e) {
+            Logger.getLogger(RentopolisController.class.getName()).log(Level.SEVERE, e.getMessage(), e);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable int id) {
+        try {
+            return new ResponseEntity<>(rentopolisServices.getUserById(id), HttpStatus.ACCEPTED);
+        } catch (RentopolisServicesException e) {
+            Logger.getLogger(RentopolisController.class.getName()).log(Level.SEVERE, e.getMessage(), e);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+}
